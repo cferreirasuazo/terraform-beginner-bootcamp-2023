c8a6fe80f1d2ab3dc9989273f5fce1670e35f1f4
Cristhian Antonio Ferreira Suazo <cferreirasuazo@gmail.com>
2023-10-03 00:26:14 +0000

    Add TOC readme

--- main.tf
+++ main.tf
@@ -1,43 +1,18 @@
-terraform {
-  cloud {
-    organization = "terraform-bootcamp-cristhian"
-    workspaces {
-      name = "terraform-bootcamp-workspace"
-    }
-  }
-
-  required_providers {
-    random = {
-      source = "hashicorp/random"
-      version = "3.5.1"
-    }
-
-    aws = {
-      source = "hashicorp/aws"
-      version = "5.19.0"
-    }
-  }
-}
-
-provider "aws" {
-   
-}
-
-provider "random" {
-  # Configuration options
-}
-
+# https://registry.terraform.io/providers/hashicorp/random/latest/docs/resources/string
 resource "random_string" "bucket_name" {
   lower = true
   upper = false
-  length   = 16
+  length   = 32
   special  = false
 }
 
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/s3_bucket
 resource "aws_s3_bucket" "example" {
+  # Bucket Naming Rules
+  #https://docs.aws.amazon.com/AmazonS3/latest/userguide/bucketnamingrules.html?icmpid=docs_amazons3_console
   bucket = random_string.bucket_name.result
-}
 
-output "random_bucket_name" {
-  value = random_string.bucket_name.result
+  tags = {
+    UserUuid = var.user_uuid
+  }
 }
